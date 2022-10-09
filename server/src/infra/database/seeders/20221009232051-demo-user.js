73040b72e7b46b08cb003b4205de8832791e7a5a
Antonio Thiele <athiele@multiplan.com.br>
2022-10-09 20:28:36 -0300

    user seed

---
+++ server/src/infra/database/seeders/20221009232051-demo-user.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [{
+      userName: 'John Doe',
+      email: 'johndoe@test.com',
+      birthDate: new Date(),
+      //Password: test for JWT_SECRET="123"
+      password: "$2a$06$YXNcg32yY7bigC103jDGSeAmtag4cCrhSbN75WH5rVX9jYvLL1AyS",
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
