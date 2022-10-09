16f50e562ecd3454906a520f07ceac1ce38343bc
Antonio Thiele <athiele@multiplan.com.br>
2022-10-09 16:22:46 -0300

    refact ChatRoomProvider

--- client/src/hooks/chatRoom.jsx
+++ client/src/hooks/chatRoom.jsx
@@ -1,23 +1,23 @@
-import React, { createContext, useCallback, useState, useContext } from 'react';
-import { useQuery } from '@apollo/client';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import { useLazyQuery } from '@apollo/client';
 import { GET_CHATROOMS } from '../graphql';
 
 const ChatRoomContext = createContext();
 
 const ChatRoomProvider = ({ children }) => {
+  const [chatRooms, setChatRooms] = useState();
 
-  const [chatRooms, setChatRooms] = useState(() => {
-      const { data } = useQuery(GET_CHATROOMS);
-      return data.getChatRooms;
+  const [getChatRooms] = useLazyQuery(
+    GET_CHATROOMS, {
+    onCompleted: (data) => {
+      setChatRooms(data.getChatRooms);
     }
+  }
   );
 
-  //TODO: implement subscriptions on chatroom creation
-  //then transform this on a useEffect
-  const getChatRooms = useCallback(() => {
-    const { data } = useQuery(GET_CHATROOMS);
-    setChatRooms(data.getChatRooms);
-  }, [setChatRooms])
+  useEffect(() => {
+    getChatRooms();
+  }, [])
 
   return (
     <ChatRoomContext.Provider value={{
