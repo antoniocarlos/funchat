beda7244489becf4451e48eacb7d5fcf2b416dcf
Antonio Thiele <athiele@multiplan.com.br>
2022-10-10 10:46:10 -0300

    database env

--- server/src/infra/database/config/database.js
+++ server/src/infra/database/config/database.js
@@ -1,9 +1,10 @@
+require('dotenv').config();
 module.exports = {
   development: {
-    username: 'root',
-    password: 'root',
-    database: 'funchat',
-    host: 'localhost',
+    username: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    host: process.env.MYSQL_HOSTS,
     dialect: 'mysql',
   },
   test: {
