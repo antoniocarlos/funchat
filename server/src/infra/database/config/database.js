7084a51ae11a1cf763808ce3280e1019d0132c64
Antonio Thiele <athiele@multiplan.com.br>
2022-10-09 21:11:34 -0300

    localhost  database

--- server/src/infra/database/config/database.js
+++ server/src/infra/database/config/database.js
@@ -10,14 +10,14 @@ module.exports = {
     username: 'root',
     password: null,
     database: 'database_test',
-    host: '127.0.0.1',
+    host: 'localhost',
     dialect: 'mysql',
   },
   production: {
     username: 'root',
     password: null,
     database: 'database_production',
-    host: '127.0.0.1',
+    host: 'localhost',
     dialect: 'mysql',
   },
 };
